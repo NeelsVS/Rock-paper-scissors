7564b05ec348ae62c83fd6f97c7344e30c47e865
Neels <rcjavelin@gmail.com>
2024-12-16 12:42:11 +0200

    Created code for computer to choose between Rock, paper or scissors.

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-console.log("Hi")
+function getComputerChoice(){ //function for computer to pick between rock, paper or scissors.
+
+    const getComputerChoice = Math.floor(Math.random()*3);
+
+    if (getComputerChoice == 0){
+       return "Rock";
+    } else if(getComputerChoice == 1){
+        return "Paper";
+    } else if(getComputerChoice == 2){
+       return  "Scissors";
+    }
+
+    return getComputerChoice;
+}
+
+console.log(getComputerChoice())
